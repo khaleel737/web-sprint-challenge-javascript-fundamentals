ab08b7d6a2a21736641dba52b3321d97e3093a48
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2021-10-15 12:59:38 -0700

    took me 1 hour, thank you lambda

--- index.js
+++ index.js
@@ -226,12 +226,23 @@ constructor(attr){
   this.width = attr.width;
   this.height = attr.height;
 }
+volume() {
+  return (this.length * this.width * this.height);
+}
+surfaceArea() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
 }
 
+const cuboidTwo = new CuboidMakerTwo({
+  length: 4, 
+  width: 5, 
+  height: 5
+})
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
 
 
